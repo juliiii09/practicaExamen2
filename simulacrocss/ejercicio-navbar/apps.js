de4eab1d787dc2e21825573f0a5507d3ea2778d4
juliiii09 <julivalenciaa09@gmail.com>
2025-12-18 14:02:02 -0500

    Add files via upload

---
+++ simulacrocss/ejercicio-navbar/apps.js
@@ -0,0 +1,8 @@
+<script>
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () ={
+        navLinks.classList.toggle('active')
+    });
+</script>
